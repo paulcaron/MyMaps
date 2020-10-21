89197752d5a7c2c584ea8f8fea6dcf9ae5234d8a
paulcaron <paul.caron@polytechnique.edu>
2020-10-21 14:00:06 -0700

    Create a new map part 2

--- app/src/main/java/edu/stanford/pcaron/mymaps/CreateMapActivity.kt
+++ app/src/main/java/edu/stanford/pcaron/mymaps/CreateMapActivity.kt
@@ -1,11 +1,15 @@
 package edu.stanford.pcaron.mymaps
 
+import android.app.Activity
 import android.content.Context
 import android.content.DialogInterface
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
@@ -19,6 +23,8 @@ import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
 import com.google.android.material.snackbar.Snackbar
+import edu.stanford.pcaron.mymaps.models.Place
+import edu.stanford.pcaron.mymaps.models.UserMap
 
 private const val TAG = "CreateMapActivity"
 
@@ -45,6 +51,30 @@ class CreateMapActivity : AppCompatActivity(), OnMapReadyCallback {
         }
     }
 
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_create_map, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Check that 'item' is the save menu option
+        if (item.itemId == R.id.miSave) {
+            Log.i(TAG, "Tapped on save!")
+            if(markers.isEmpty()){
+                Toast.makeText(this, "There must be at least one marker on the map", Toast.LENGTH_LONG).show()
+                return true
+            }
+            val places = markers.map {marker -> Place(marker.title, marker.snippet, marker.position.latitude, marker.position.longitude) }
+            val userMap = UserMap(intent.getStringExtra(EXTRA_MAP_TITLE) as String, places)
+            val data = Intent()
+            data.putExtra(EXTRA_USER_MAP, userMap)
+            setResult(Activity.RESULT_OK, data)
+            finish()
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
     /**
      * Manipulates the map once available.
      * This callback is triggered when the map is ready to be used.
@@ -69,9 +99,8 @@ class CreateMapActivity : AppCompatActivity(), OnMapReadyCallback {
         }
 
         // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        val siliconValley = LatLng(37.4, -122.1)
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(siliconValley, 10f))
     }
 
     private fun showAlertDialog(latLng: LatLng) {
--- app/src/main/java/edu/stanford/pcaron/mymaps/MainActivity.kt
+++ app/src/main/java/edu/stanford/pcaron/mymaps/MainActivity.kt
@@ -1,11 +1,18 @@
 package edu.stanford.pcaron.mymaps
 
 import android.app.Activity
+import android.content.DialogInterface
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.LayoutInflater
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 import edu.stanford.pcaron.mymaps.models.Place
 import edu.stanford.pcaron.mymaps.models.UserMap
 import kotlinx.android.synthetic.main.activity_main.*
@@ -16,15 +23,19 @@ private const val REQUEST_CODE = 1234
 private const val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var userMaps: MutableList<UserMap>
+    private lateinit var mapAdapter: MapsAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val userMaps = generateSampleData()
+        userMaps = generateSampleData().toMutableList()
         // Set layout manager on the recycler view
         rvMaps.layoutManager = LinearLayoutManager(this)
         // Set adapter on the recycler view
-        rvMaps.adapter = MapsAdapter(this, userMaps, object: MapsAdapter.OnClickListener {
+        mapAdapter = MapsAdapter(this, userMaps, object: MapsAdapter.OnClickListener {
             override fun onItemClick(position: Int) {
                 Log.i(TAG, "onItemClick $position")
                 // When user taps on view in RV, navigate to new activity
@@ -33,18 +44,46 @@ class MainActivity : AppCompatActivity() {
                 startActivity(intent)
             }
         })
+        rvMaps.adapter = mapAdapter
 
         fabCreateMap.setOnClickListener {
             Log.i(TAG, "Tap on FAB")
-            val intent = Intent(this@MainActivity, CreateMapActivity::class.java)
-            intent.putExtra(EXTRA_MAP_TITLE, "new map name")
-            startActivityForResult(intent, REQUEST_CODE)
+            showAlertDialog()
         }
+    }
+
+    private fun showAlertDialog() {
+        val mapFormView = LayoutInflater.from(this).inflate(R.layout.dialog_create_map, null)
+        val dialog =
+            AlertDialog.Builder(this)
+                .setTitle("Map title")
+                .setView(mapFormView)
+                .setNegativeButton("Cancel", null)
+                .setPositiveButton("OK", null)
+                .show()
+        dialog.getButton(DialogInterface.BUTTON_POSITIVE).setOnClickListener {
+            val title = mapFormView.findViewById<EditText>(R.id.etTitle).text.toString()
+            if (title.trim().isEmpty()) {
+                Toast.makeText(this, "Map must have a non-empty title", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            // Navigate to create map activity
+            val intent = Intent(this@MainActivity, CreateMapActivity::class.java)
+            intent.putExtra(EXTRA_MAP_TITLE, title)
+            startActivityForResult(intent, REQUEST_CODE)
+            dialog.dismiss()
+        }
+
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         if (requestCode == REQUEST_CODE && resultCode == Activity.RESULT_OK) {
             // Get new map data from the data
+            val userMap = data?.getSerializableExtra(EXTRA_USER_MAP) as UserMap
+            Log.i(TAG, "onActivityResult with new map title ${userMap.title}")
+            userMaps.add(userMap)
+            mapAdapter.notifyItemInserted(userMaps.size-1)
         }
         super.onActivityResult(requestCode, resultCode, data)
     }
